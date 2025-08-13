bc57d60b57d5f7d60ea4b188965c43e05ddcfc4c
Hesham Elsayed <93478673+Hesham749@users.noreply.github.com>
2025-08-14 00:45:07 +0300

    Delete CSharp/Concatenation-of-Array.cs

--- CSharp/Concatenation-of-Array.cs
+++
@@ -1,7 +0,0 @@
-public class Solution {
-    public int[] GetConcatenation(int[] nums) {
-        var concatination = new List<int>(nums);
-        concatination.AddRange(nums);
-        return [..concatination];
-    }
-}
