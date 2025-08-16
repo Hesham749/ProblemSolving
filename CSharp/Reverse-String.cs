29c09341ed9d11ba34df9e6386603aa9c4e00d46
Hesham Elsayed <93478673+Hesham749@users.noreply.github.com>
2025-08-16 23:55:47 +0300

    [344] [Time Beats: 100.00%] [Memory Beats: 82.74%] - LeetPush

---
+++ CSharp/Reverse-String.cs
@@ -0,0 +1,8 @@
+public class Solution {
+    public void ReverseString(char[] s) {
+        for (int i = 0; i < s.Length / 2; i++)
+            {
+                (s[i], s[s.Length - i -1]) = (s[s.Length - i-1], s[i]);
+            }
+    }
+}
