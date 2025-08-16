7777ad85f46d292e3c6893a79358a8e2a3da5822
Hesham Elsayed <93478673+Hesham749@users.noreply.github.com>
2025-08-17 01:58:06 +0300

    Update Two-Sum.cs

--- CSharp/Two-Sum.cs
+++ CSharp/Two-Sum.cs
@@ -1,24 +1,16 @@
 public class Solution {
     public int[] TwoSum(int[] nums, int target) {
 	Dictionary<int, int> valueIndex = new();
-	int[] twosum = new int[2];
 	for (int i = 0; i < nums.Length; i++)
 	{
 
 		int secondNum = target - nums[i];
 		if (valueIndex.TryGetValue(secondNum, out int index))
 		{
-			if (i != index)
-			{
-
-				twosum[0] = i;
-				twosum[1] = index;
-				return twosum;
-			}
+			return [i, index];
 		}
-		if (!valueIndex.TryAdd(nums[i], i))
-			valueIndex[nums[i]] = i;
+		valueIndex.TryAdd(nums[i], i);
 	}
-	return twosum;
+	return [];
     }
 }
