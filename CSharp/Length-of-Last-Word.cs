25d15dbc482851f792ad9a8be05fd7c9085c1294
Hesham Elsayed <93478673+Hesham749@users.noreply.github.com>
2025-08-17 02:48:26 +0300

    [58] [Time Beats: 100.00%] [Memory Beats: 90.70%] - LeetPush

---
+++ CSharp/Length-of-Last-Word.cs
@@ -0,0 +1,17 @@
+public class Solution {
+    public int LengthOfLastWord(string s) {
+	int length = 0;
+	for (int i = s.Length - 1; i >= 0; i--)
+	{
+		if (char.IsLetter(s[i]))
+		{
+			length++;
+		}
+		else if (length > 0)
+		{
+			break;
+		}
+	}
+	return length;
+    }
+}
