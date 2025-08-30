03691ffb66a5a3d3d78b3ca20fb3f1a1aaa7a1b9
Hesham Elsayed <93478673+Hesham749@users.noreply.github.com>
2025-08-31 01:59:51 +0300

    [347] [Time Beats: 38.84%] [Memory Beats: 78.54%] - LeetPush

---
+++ CSharp/Top-K-Frequent-Elements.cs
@@ -0,0 +1,24 @@
+public class Solution {
+    public int[] TopKFrequent(int[] nums, int k) {
+	var freq = new Dictionary<int, int>();
+
+	if (k == nums.Length)
+		return nums;
+
+	for (int i = 0; i < nums.Length; i++)
+	{
+		int current = nums[i];
+		if (freq.ContainsKey(current))
+		{
+			freq[current]++;
+		}
+		else
+		{
+			freq.Add(current, 1);
+
+		}
+	}
+
+	return [.. freq.OrderByDescending(x => x.Value).Take(k).Select(x => x.Key)];
+    }
+}
